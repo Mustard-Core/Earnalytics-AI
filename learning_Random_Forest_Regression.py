9cfada8f2e2432d9515ff021da7cdd5b5013a990
Tshepang Phakani <tshepangPhakani01@gmail.com>
2025-10-12 14:30:28 +0200

    I deleted the learning RF regression model

--- learning_Random_Forest_Regression.py
+++ learning_Random_Forest_Regression.py
@@ -11,12 +11,12 @@ X = X.to_numpy()
 y = df['Salary']
 
 #Train test split
-test_size = 70
+test_size = 55
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = test_size/100, random_state = 42)
 
 #Logistic regression
 model = LinearRegression()
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+#model = RandomForestRegressor(n_estimators=100, random_state=42)
 
 model = model.fit(X_train, y_train)
 y_test_pred = model.predict(X_test)
